d2ae1f055f878e09b8be0eeffa8ead5bb39c5336
Sudarshan L <passinthrunow@gmail.com>
2022-02-08 14:52:54 +0530

    Remove deprecated components

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask
--- setup.sh
+++ setup.sh
@@ -1,4 +1,2 @@
 brew tap Homebrew/bundle
-brew tap caskroom/versions
 brew bundle -v
-brew tap homebrew/completions
