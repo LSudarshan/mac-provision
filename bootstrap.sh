73679c94bb8c39b04f4d9772d3660139e69567ec
Sudarshan L <sudarsan@e6x.io>
2022-06-28 10:00:15 +0530

    Add maven

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
